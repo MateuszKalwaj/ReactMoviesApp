3c971e829cdc6bb4ba26e0989683c8bd77ce2388
mateusz_kalwaj <tynozobacz@gmail.com>
2020-04-19 00:14:06 +0200

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <div>
+        <Navbar />;
+        <Footer />;
+      </div>
+    ) 
   }
 };
 
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+           <footer class="page-footer font-small blue">
+
+
+<div class="footer-copyright text-center py-3">© 2020 Copyright: King of this site
+  <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+</div>
+
+
+</footer>
+        </div>
+    )
+}
+
+export default Footer
