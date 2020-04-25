515dec96c120846ce392677b10178c62653bee22
mateusz_kalwaj <tynozobacz@gmail.com>
2020-04-25 18:57:29 +0200

    Updates

---
+++ precache-manifest.2982f80a1527fdb7e67d62ad58f2d4fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c636d8679f05dbe022823d0b1d84c18b",
+    "url": "/react-movies-app/index.html"
+  },
+  {
+    "revision": "e0432eb99b2c303e8e92",
+    "url": "/react-movies-app/static/css/main.c0cdbc7f.chunk.css"
+  },
+  {
+    "revision": "4876582e6dcee88a6e04",
+    "url": "/react-movies-app/static/js/2.dc788824.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies-app/static/js/2.dc788824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0432eb99b2c303e8e92",
+    "url": "/react-movies-app/static/js/main.5ad48b06.chunk.js"
+  },
+  {
+    "revision": "03e06e9e8ceb1834b069",
+    "url": "/react-movies-app/static/js/runtime-main.112d0e86.js"
+  },
+  {
+    "revision": "772cfa1733116a13b327edfdb5c1fe8e",
+    "url": "/react-movies-app/static/media/PacMan.772cfa17.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactMoviesApp/precache-manifest.50701f899cc6eac0d0510b60aa9575d1.js"
+  "/react-movies-app/precache-manifest.2982f80a1527fdb7e67d62ad58f2d4fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactMoviesApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movies-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
